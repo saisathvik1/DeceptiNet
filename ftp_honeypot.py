37e432a2c67fb11c7bb39177bbebef91928979dd
Sai Sathvik Ruppa <51957422+saisathvik1@users.noreply.github.com>
2023-08-21 12:25:33 +0530

    Update ftp_honeypot.py
    Made changes to the logging mechanism.

--- ftp_honeypot.py
+++ ftp_honeypot.py
@@ -1,8 +1,10 @@
 import socket
+import datetime
+import os
 
 def ftp_honeypot():
     host = "0.0.0.0"  # Listen on all available interfaces
-    port = 21         # FTP port, port can be changed.
+    port = 21         # FTP port
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((host, port))
@@ -14,7 +16,10 @@ def ftp_honeypot():
             client_socket, client_address = server.accept()
             print(f"Connection from {client_address[0]}:{client_address[1]}")
 
-            client_socket.send(b"220 (vsFTPd 2.3.4)\r\n")  # It currently shows as VSFTPD 2.3.4, but it can be changed.
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log_message = f"[{current_time}] Connection from {client_address[0]}:{client_address[1]}"
+
+            client_socket.send(b"220 (vsFTPd 2.3.4)\r\n")  # Modify the banner here
 
             while True:
                 data = client_socket.recv(1024).decode().strip()
@@ -23,21 +28,22 @@ def ftp_honeypot():
                     break
 
                 print(f"Received: {data}")
+                log_message += f"\nReceived: {data}"
 
                 if "USER anonymous" in data:
                     client_socket.send(b"331 Please specify the password.\r\n")
                 elif "PASS" in data:
                     client_socket.send(b"230 Login successful.\r\n")
 
-                    with open("ftp_log.txt", "a") as log_file:
-                        log_file.write(f"Connection from {client_address[0]}:{client_address[1]} - Anonymous login\n")
+                # ... rest of your code ...
 
-                elif "QUIT" in data:
-                    client_socket.send(b"221 Goodbye.\r\n")
-                    client_socket.close()
-                    break
-                else:
-                    client_socket.send(b"530 Login incorrect.\r\n")
+            # Log every connection attempt
+            if not os.path.exists("ftp_log.log"):
+                with open("ftp_log.log", "w") as log_file:
+                    pass  # Create an empty file
+
+            with open("ftp_log.log", "a") as log_file:
+                log_file.write(f"{log_message}\n")
 
             client_socket.close()
 
